a4bbc83609e5ec32b33f0c2d7e93e23789dd3d86
Anton Khoroshaev <vax@site.com>
2023-03-16 21:47:53 +0300

    Examples 5 & 6 If-Else, Поиск максимума

---
+++ Lesson-01/Example005_IfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Представьтесь, пожалуйста: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маня"){
+    Console.WriteLine("Я Вас категорически приветствую!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
---
+++ Lesson-01/Example006_Max/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 5;
+int c = 2;
+int d = 9;
+int e = 0;
+int max = 0;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine(max);
