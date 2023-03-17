7ac6470e48d47e30dbd639a290378210bbd79935
Anton Khoroshaev <vax@site.com>
2023-03-17 21:36:24 +0300

    Домашнее задание к Семинару 1 (Задача 1)

---
+++ Lesson-02/Task-010/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трёхначное число: ");
+string N = Console.ReadLine();
+int n = System.Convert.ToInt32(N);
+
+int res2 = n / 10;
+int dig2 = res2 % 10;
+Console.WriteLine(dig2);
